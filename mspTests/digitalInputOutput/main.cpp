392d023c4ab57214552cd5c850654d3d4c676a74
David Kennedy <daviduser@me.com>
2014-10-05 21:47:08 -0300

    better comments to digital IO msp example

--- mspTests/digitalInputOutput/main.cpp
+++ mspTests/digitalInputOutput/main.cpp
@@ -1,34 +1,30 @@
 #include <msp/msp430g2553.h>
 
+/** Description:
+ * change leds on while you press button
+ * display green light until press button,
+ * then display red light while press button
+ */
+
 main() {
-	WDTCTL = WDTPW + WDTHOLD;
+	P1SEL |= 0x00; //no special function of pins on port P1
+	P1DIR |= BIT0 | BIT6; // configure output mode pins 0 and 6, corresponding to leds 1 (red) and 2 (green)
+	P1REN |= BIT3; // configure pin 3 (switch 2 SW2) who pullup, because when pressed the switch pull GND on wire
 
-	P1SEL |= 0x00;
-	P1DIR |= BIT0 | BIT6; // configure the pin 0 of port 1 to output, other's are in input mode
-	P1REN |= BIT3; // configure switch 2 who pullup
+	while (true)
+		if (P1IN & BIT3) { // if button isn't pressed (pin not grounded, so have pin value 1)
+			P1OUT |= BIT6; // set pin 6 on
+			P1OUT &= ~BIT0; // set pin 0 off
+		} else
+			//exemplo de inline assembly
+			asm(
+				"bis %[pin0], %0;"	// set pin 0 on
+				"bic %[pin6], %0"	// set pin 6 off
 
-#define ASM_CMD(INS, SRC, DEST) asm( #INS" %1, %0;": "=r" (DEST): "i" (SRC) );
-#define BIS(SRC, DEST) ASM_CMD(bis.b, SRC, DEST)
-#define BIC(SRC, DEST) ASM_CMD(bic.b, SRC, DEST)
-
-#define BS asm( "bis.b %1, %0;": "=r" (
-#define _BS ): "i" (
-#define BS_ ) );
-
-	BS P1OUT _BS BIT0 BS_
-	BIS(BIT6, P1OUT)
-
-	
-/*
-	while (true) {
-		if (P1IN & BIT3) {
-			//P1OUT |= BIT6;
-			P1OUT &= ~BIT0;
-		} else {
-			P1OUT |= BIT0;
-			P1OUT &= ~BIT6;
-		}
-	}
-*/
-		
+				: // parametros de escrita (rvalue)
+					"=r" (P1OUT) // parametro %0 , poderia ser entitulado de forma mais legivel
+				: // parametros de leitura (lvalue)
+					[pin0] "i" (BIT0), //parametro %1 ou %[pin0] ("i" diz que o parametro BIT0 é uma constante)
+					[pin6] "i" (BIT6) //parametro %2 ou %[pin6]
+			);
 }
